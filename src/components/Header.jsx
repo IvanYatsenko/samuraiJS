39eb855bd3b18101fda44f53fa0d1c13d49f8c25
yatsenko.i <yatsenko.ivan@rambler.ru>
2023-11-07 21:51:45 +0700

    12 lesson

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        alt="logo"
+        src="https://img.freepik.com/premium-vector/minimal-line-design-logo-connection-icon_205544-1071.jpg"
+      />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Profile from './Profile'
+
+const Main = () => {
+  return (
+    <main className="main">
+      <Profile />
+    </main>
+  )
+}
+
+export default Main
